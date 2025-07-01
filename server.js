2fab0b55f0a95d83c630c47383170e9cdd0b9128
Myrzabek Kaisar <myrzabekkajsar89@gmail.com>
2025-07-01 12:22:34 +0500

    dfsdf

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get("/books", async (req, res) => {
 app.get("/books/:id", async (req, res) => {
   const { id } = req.params;
   try {
-    const result = await pool.query("SELECT * FROM books WHERE id = $1", [id]);
+    const result = await pool.query("SELECT * FROM books WHERE id = 1", [id]);
     res.json(result.rows[0]);
   } catch (err) {
     res.status(500).json({ error: "qate" });
