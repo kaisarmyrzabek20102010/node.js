0ba53a21896d7a78286ea46accd328165d1204d2
Myrzabek Kaisar <myrzabekkajsar89@gmail.com>
2025-07-16 16:26:31 +0500

    dsvsdv

--- server.js
+++ server.js
@@ -1,63 +1,100 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const pool = require('./pool'); 
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const rateLimit = require("express-rate-limit");
+const pool = require("./db");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+app.use(helmet());
 
-const JWT_SECRET = 'secret123';
- 
+const SECRET_KEY = "secret123";
+const PORT = 3000;
+
+// const loginLimiter = rateLimit({
+//   windowMs: 720000,
+//   max: 3,
+//   message: {
+//     status: 429,
+//     error: 'Көп сұраныс жасадыңыз. Сізге 2 сағ бан берілді.',
+//   }
+// });
+
 function authMiddleware(req, res, next) {
   const authHeader = req.headers.authorization;
-  const token = authHeader && authHeader.split(' ')[1];
 
-  if (!token) {
-    return res.status(400).json({ message: 'Token керек' });
-  }
+  if (!authHeader) return res.status(401).json({ message: "Token жоқ" });
 
+  const token = authHeader.split(" ")[1];
   try {
-    const decoded = jwt.verify(token, JWT_SECRET);
+    const decoded = jwt.verify(token, SECRET_KEY);
     req.user = decoded;
     next();
-  } catch (err) {
-    return res.status(404).json({ message: 'qate' });
+  } catch {
+    res.status(401).json({ message: "Token жарамсыз" });
   }
 }
- 
-app.post('/login', (req, res) => {
-  const user = { id: 1, name: 'TestUser' };
-  const token = jwt.sign(user, JWT_SECRET, { expiresIn: '1h' });
-  res.json({ token });
-});
- 
-app.post('/products', authMiddleware, async (req, res) => {
-  const { name, price, quantity } = req.body;
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  const hashed = await bcrypt.hash(password, 10);
 
   try {
-    const result = await pool.query(
-      'INSERT INTO products (name, price, quantity) VALUES ($1, $2, $3) *',
-      [name, price, quantity]
-    );
-    res.json(result.rows[0]);
+    await pool.query("INSERT INTO users(username, password) VALUES($1, $2)", [
+      username.toLowerCase(),
+      hashed,
+    ]);
+    res.json({ message: "Тіркелу сәтті өтті" });
   } catch (err) {
-    res.status(404).json({ message: 'qate'});
+    res.status(500).json({ error: "Пайдаланушыны тіркеу кезінде қате болды" });
   }
 });
- 
-app.get('/products', authMiddleware, async (req, res) => {
+
+app.post("/login", loginLimiter, async (req, res) => {
+  const { username, password } = req.body;
+
   try {
-    const result = await pool.query('SELECT * FROM products');
-    res.json(result.rows);
+    const result = await pool.query("SELECT * FROM users WHERE username=$1", [
+      username.toLowerCase(),
+    ]);
+
+    const user = result.rows[0];
+    if (!user)
+      return res.status(401).json({ error: "Қате логин немесе құпия сөз" });
+
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid)
+      return res.status(401).json({ error: "Қате логин немесе құпия сөз" });
+
+    const token = jwt.sign(
+      { id: user.id, username: user.username },
+      SECRET_KEY,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    res.json({ token });
   } catch (err) {
-    res.status(404).json({ message: 'qate'});
+    console.log(err);
+    res.status(500).json({ error: " login қате болды" });
   }
 });
- 
-app.get('/profile', authMiddleware, (req, res) => {
-  res.json({ message: 'hi   ' });
+
+app.get("/profile", authMiddleware, async (req, res) => {
+  try {
+    const code = jwt.verify(token);
+    req.user = code;
+    next();
+    res.json({ message: `welcome, ${req.user.username}` });
+  } catch (error) {
+
+  }
 });
- 
-app.listen(3000, () => {
-  console.log('aqwerty');
+
+app.listen(PORT, () => {
+  console.log(`Сервер http://localhost:${PORT} портында жұмыс істеуде`);
 });
